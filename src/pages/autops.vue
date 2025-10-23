deb8ca3a564c07489a5d9bd5e29bf0954199676b
zhang-leleer <12440130+zhang-leleer@user.noreply.gitee.com>
2025-10-23 19:54:53 +0800

    feature: 更改菜单样式，更改群图片

--- src/pages/autops.vue
+++ src/pages/autops.vue
@@ -295,7 +295,7 @@
       </v-card-title>
       <v-card-text class="text-center">
         <v-img
-          src="https://lyle-mac.oss-cn-hangzhou.aliyuncs.com/blog/1774d050acf2fa9f94a7cf145a3065f9.jpg"
+          src="https://lyle-mac.oss-cn-hangzhou.aliyuncs.com/blog/9ea5b5ed22e4131f1f566eb3b95812d6.jpg"
           alt="假条交流群"
           max-width="250"
           class="mx-auto"
---
+++ src/pages/notion/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="notion-page">
+    <iframe src="https://yoleo.notion.site/ebd/1b5a9ac6118e8030b238cf4638cd539b" width="100%" height="1000" frameborder="0" allowfullscreen />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<style scoped>
+
+</style>
